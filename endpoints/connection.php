43fd945ef7f61f194b92f5e41093a44f3048f4c7
GizakiF <aranetagian4221@gmail.com>
2025-04-26 19:27:31 +0800

    ensuring commit

--- endpoints/connection.php
+++ endpoints/connection.php
@@ -1,11 +1,14 @@
 <?php
 
+session_start();
+
 $serverName = "localhost";
 $userName = "root";
 $password = "";
+$db = "troolifedb";
 
 try {
-    $conn = new mysqli($serverName, $userName, $password);
+    $conn = new mysqli($serverName, $userName, $password, $db);
     return $conn;
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
