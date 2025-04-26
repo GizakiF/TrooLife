514a97b3f6f253c393eaac23bd34658118dd6984
GizakiF <aranetagian4221@gmail.com>
2025-04-26 19:37:11 +0800

    change the url to reflect the name change of login service

--- login_page.php
+++ login_page.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$url = "http://localhost:8000/endpoints/authentication/login.php";
+$url = "http://localhost:8000/endpoints/authentication/login_service.php";
 $errors = $_SESSION['login_errors'] ?? [];
 unset($_SESSION['login_errors']);
 ?>
